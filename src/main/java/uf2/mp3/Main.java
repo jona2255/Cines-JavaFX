8b5db9df5ccc6770968d45a29659b3c77bd8366f
prog2 <prog2@microdigit.es>
2020-02-21 10:32:05 +0100

    Changed fxml file on Main execution

--- src/main/java/uf2/mp3/Main.java
+++ src/main/java/uf2/mp3/Main.java
@@ -5,16 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import uf2.mp3.control.ChartExampleController;
 import uf2.mp3.control.MainWindow;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/mainWindow.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/chartExample.fxml"));
         AnchorPane root = loader.load();
-        MainWindow mainWindow = loader.getController();
-        mainWindow.setStage(stage);
+        ChartExampleController chartExampleController = loader.getController();
+        chartExampleController.setStage(stage);
+//        MainWindow mainWindow = loader.getController();
+//        mainWindow.setStage(stage);
         stage.setTitle("testFX");
         Scene sc = new Scene(root);
         sc.getStylesheets().add("css/styleCharts.css");
--- src/main/java/uf2/mp3/control/ChartExampleController.java
+++ src/main/java/uf2/mp3/control/ChartExampleController.java
@@ -1,79 +1,253 @@
 package uf2.mp3.control;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.*;
-import javafx.scene.control.TabPane;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ChartExampleController implements Initializable {
-    private ObservableList<PieChart.Data> dataCharts;
-    private List<Integer> dadesInts = new ArrayList<>();
+    private final ObservableList<PieChart.Data> dataCharts = FXCollections.observableArrayList();
+    private Stage stage;
+
+
+    ConexionXML conexionXML;
+    List<String> images;
+    String url = "http://www.gencat.cat/llengua/cinema/";
+
+    List<Film> films;
+    List<Sesion> sesions;
+    List<Cinema> cinemas;
+    List<Cicle> cicles;
+
+    private double x=0, y=0;
+
+    ObservableList<String> listObservableFilms =FXCollections.observableArrayList();
+    ObservableList<Sesion> listObservableSesions =FXCollections.observableArrayList();
+
 
     @FXML
-    PieChart pieChart01;
-    @FXML
-    BarChart<?,?> barChart01;
-    @FXML
-    private CategoryAxis x;
-    @FXML
-    private NumberAxis y;
+    private ListView<String> listViewFilms;
 
     @FXML
-    ImageView img01;
+    private Text textTitleFilm;
+
+    @FXML
+    private ImageView imageFilm;
+
+    @FXML
+    private Circle btnCerrar;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Text direcctorFilm;
+
+    @FXML
+    private Text directorTitle;
+
+    @FXML
+    private Text añoFilm;
+
+    @FXML
+    private Text añoTitle;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private Button buttonSesion;
+
+    @FXML
+    private AnchorPane  paneSesion;
+
+    @FXML Text sesionTitle;
+
+    @FXML
+    private TableColumn<Sesion, String> tableColumnTitleCinema;
+
+    @FXML
+    private TableView<Sesion> tableViewSesiones;
+
+    @FXML
+    private TableColumn<Sesion, String> tableColumnSesion;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        dataCharts = FXCollections.observableArrayList();
-        loadDataPieChart();
-        pieChart01.setData(dataCharts);
-        pieChart01.setTitle("Llenguatges de programació més usats");
-
-        loadDataBarChart();
-        barChart01.setTitle("Salari anual");
-        x.setLabel("Llenguatges");
-        y.setLabel("Euros");
-
-        img01.setImage(new Image("http://gencat.cat/llengua/cinema/docsbarcelonadelmes_cicle.jpg"));
+        try {
+            pane.setVisible(false);
+            connectedXML();
+            loadFilms();
+            makeDragable();
+            opaqueInfoMovie();
+            diagrama();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadDataPieChart() {
-        dataCharts.add(new PieChart.Data("Java",30));
-        dataCharts.add(new PieChart.Data("C#",25));
-        dataCharts.add(new PieChart.Data("Python", 25));
-        dataCharts.add(new PieChart.Data("Ruby",10));
-        dataCharts.add(new PieChart.Data("R",10));
+    private void diagrama(){
+        List<Integer> años = films.stream()
+                .map(film -> film.getAny())
+                .filter(i -> i > 0 && i < 3000).distinct()
+                .sorted(Comparator.comparingInt(integer -> integer))
+                .collect(Collectors.toList());
+
+        for (Integer i: años) {
+            long numResultat= films.stream()
+                    .filter(film1 -> film1.getAny() == i)
+                    .count();
+            dataCharts.add(new PieChart.Data(i.toString(), numResultat));
+        }
+
+        pieChart.setData(dataCharts);
+        pieChart.setLegendSide(Side.LEFT);
+
+        final Label label = new Label();
+        pane.getChildren().add(label);
+        label.setFont(Font.font("SanSerif", FontWeight.BLACK, 20));
+
+        pieChart.getData().stream().forEach(data -> {
+            data.getNode().addEventHandler(MouseEvent.ANY, e->{
+                int intValue = (int) data.getPieValue();
+                pane.setVisible(true);
+                if(intValue==1){
+                    label.setText(intValue + " pelicula");
+                }else {
+                    label.setText(intValue + " peliculas");
+                }
+            });
+        });
     }
 
-    private void loadDataBarChart() {
-        XYChart.Series set1 = new XYChart.Series<>();
-        XYChart.Series set2 = new XYChart.Series<>();
+    private void connectedXML() throws JAXBException, IOException {
+        conexionXML = new ConexionXML();
+        conexionXML.connectedFilms();
+        conexionXML.connectedSessions();
+        conexionXML.connectedCinema();
+        conexionXML.connectedCicles();
 
-        set1.getData().add(new XYChart.Data<>("Java", 50000));
-        set1.getData().add(new XYChart.Data<>("C#", 45000));
-        set1.getData().add(new XYChart.Data<>("Python", 35000));
-        set1.getData().add(new XYChart.Data<>("Ruby", 35000));
-        set1.getData().add(new XYChart.Data<>("R", 60000));
-
-        barChart01.getData().addAll(set1);
-        set1.setName("2019");
-
-        set2.getData().add(new XYChart.Data<>("Java", 60000));
-        set2.getData().add(new XYChart.Data<>("C#", 40000));
-        set2.getData().add(new XYChart.Data<>("Python", 45000));
-        set2.getData().add(new XYChart.Data<>("Ruby", 55000));
-        set2.getData().add(new XYChart.Data<>("R", 60000));
-
-        barChart01.getData().addAll(set2);
-        set2.setName("2020");
+        sesions = conexionXML.getSesions();
+        cinemas = conexionXML.getCinemas();
+        films = conexionXML.getFilms();
+        cicles = conexionXML.getCicles();
     }
+
+    private void loadFilms() throws JAXBException, IOException {
+        conexionXML = new ConexionXML();
+        conexionXML.connectedFilms();
+
+        films = conexionXML.getFilms();
+
+        List<String> listaTitle = films.stream().map(film -> film.getTitol()).collect(Collectors.toList());
+        images = films.stream().map(film -> film.getImage()).collect(Collectors.toList());
+
+        listObservableFilms.addAll(listaTitle);
+        listViewFilms.getItems().addAll(listObservableFilms);
+    }
+
+    public void displaySelected(MouseEvent mouseEvent) {
+        String filmTitle = listViewFilms.getSelectionModel().getSelectedItem();
+
+        if(filmTitle==null|| filmTitle.isEmpty()){
+            textTitleFilm.setText("No has seleccionado ninguna pelicula");
+        } else {
+            visibleInfoMovie();
+            textTitleFilm.setText(filmTitle);
+            for (Film f: films) {
+                if(f.getTitol().equals(filmTitle)){
+                    String urlImage=url+f.getImage();
+                    Image imageMovie = new Image(urlImage);
+
+                    imageFilm.setImage(imageMovie);
+                    direcctorFilm.setText(f.getDireccio());
+                    añoFilm.setText(String.valueOf(f.getAny()));
+
+                    List<Sesion> listaCines = sesions.stream().filter(sesion -> f.getIdFilm() == sesion.getIdFilm()).collect(Collectors.toList());
+                    listObservableSesions.addAll(listaCines);
+
+//                    TableColumn cineColumn = new TableColumn("Cine");
+//                    cineColumn.setCellValueFactory(new PropertyValueFactory<Cinema, String>("cine"));
+//                    tableViewSesiones.getColumns().setAll(cineColumn);
+
+                    tableColumnTitleCinema.setCellValueFactory(new PropertyValueFactory("nomCine"));
+                    tableViewSesiones.setItems(listObservableSesions);
+
+                    sesionTitle.setText(f.getTitol());
+                }
+            }
+        }
+    }
+
+    public void handlerMouseEvent(MouseEvent mouseEvent) {
+        if(mouseEvent.getSource() == btnCerrar){
+//            System.exit(0);
+            Stage stage = (Stage) btnCerrar.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    public void makeDragable(){
+        tabPane.setOnMousePressed((event -> {
+            x=event.getSceneX();
+            y=event.getSceneY();
+        }));
+
+        tabPane.setOnMouseDragged((event -> {
+            Stage stage = (Stage) btnCerrar.getScene().getWindow();
+            stage.setX(event.getScreenX()-x);
+            stage.setY(event.getScreenY()-y);
+        }));
+    }
+
+    public void visibleInfoMovie(){
+        textTitleFilm.setVisible(true);
+        direcctorFilm.setVisible(true);
+        directorTitle.setVisible(true);
+        añoFilm.setVisible(true);
+        añoTitle.setVisible(true);
+    }
+
+    public void opaqueInfoMovie(){
+        textTitleFilm.setVisible(false);
+        direcctorFilm.setVisible(false);
+        directorTitle.setVisible(false);
+        añoFilm.setVisible(false);
+        añoTitle.setVisible(false);
+    }
+
+    public void clickFilmSesion(MouseEvent mouseEvent) {
+
+        paneSesion.toFront();
+    }
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
 }
