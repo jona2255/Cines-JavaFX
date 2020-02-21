86daa4ab56a90226cc2eca871110a5d5cfde67ee
Programador 1 <prog1@microdigit.es>
2020-02-21 10:22:06 +0100

    Objetos

---
+++ src/main/java/uf2/mp3/control/Cicle.java
@@ -0,0 +1,51 @@
+package uf2.mp3.control;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class Cicle {
+    @XmlElement(name="CICLENOM")
+    String nombre;
+
+    @XmlElement(name="CICLEID")
+    int idCiclo;
+
+    @XmlElement(name="CICLEINFO")
+    String info;
+
+    @XmlElement(name="IMGCICLE")
+    String img;
+
+    String buscar;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdCiclo() {
+        return idCiclo;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public String getBuscar() {
+        return buscar;
+    }
+
+    @Override
+    public String toString() {
+        return "Cicle{" +
+                "nombre='" + nombre + '\'' +
+                ", idCiclo='" + idCiclo + '\'' +
+                ", info='" + info + '\'' +
+                ", img='" + img + '\'' +
+                ", buscar='" + buscar + '\'' +
+                '}';
+    }
+
+}
