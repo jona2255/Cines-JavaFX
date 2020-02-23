a6729bd2934aaa99e2d800d6ab32ab2a1c940834
Jona <jona2255@gmail.com>
2020-02-23 13:08:24 +0100

    error

--- src/main/java/uf2/mp3/Main.java
+++ src/main/java/uf2/mp3/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import uf2.mp3.control.FilmsController;
 
 public class Main extends Application {
@@ -17,16 +16,21 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/filmsWindow.fxml"));
-        AnchorPane root = loader.load();
-        FilmsController filmsController = loader.getController();
-        filmsController.setStage(stage);
-        stage.setTitle("testFX");
-        Scene sc = new Scene(root);
-        sc.getStylesheets().add("css/styleCharts.css");
-        stage.setScene(sc);
-        stage.setResizable(false);
-        stage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/filmsWindow.fxml"));
+            AnchorPane root = loader.load();
+            FilmsController filmsController = loader.getController();
+            filmsController.setStage(stage);
+            stage.setTitle("testFX");
+            Scene sc = new Scene(root);
+            sc.getStylesheets().add("css/styleCharts.css");
+            stage.setScene(sc);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
 
     }
 }
